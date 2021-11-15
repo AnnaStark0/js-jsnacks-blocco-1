77bf546b388455bda1050c6eb38879ca2d3c6648
Annastark <89winteriscoming@gmail.com>
2021-11-15 19:46:15 +0100

    Snack-2 Init

---
+++ Snack-2/script.js
@@ -0,0 +1,12 @@
+//JSnack 2
+/*Dare la possibilità di inserire due parole.
+Verificare tramite una funzione che le due parole abbiano la stessa lunghezza.
+Se hanno la stessa lunghezza, stamparle entrambe altrimenti stampare la più lunga delle due.*/
+
+parolaUno = prompt('Inserisci una parola');
+
+parolaDue = prompt('Inserisci un altra parola');
+
+if (parolaUno.length == parolaDue.length) {
+    
+}
