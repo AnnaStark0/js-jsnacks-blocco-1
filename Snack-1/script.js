2b7f3b117a73625254b716d72bd6fdd410bca310
Annastark <89winteriscoming@gmail.com>
2021-11-15 19:38:31 +0100

    Snack-1 Ciclo While

--- Snack-1/script.js
+++ Snack-1/script.js
@@ -3,12 +3,31 @@
 Il programma stampa la somma di tutti i numeri inseriti.
 Esegui questo programma in due versioni, con il for e con il while*/
 
-let somma = 0;
+
+//Ciclo FOR
+
+/*let somma = 0;
 
 for (let i = 0; i <5; i++) {
     let n = parseInt(prompt('Inserisci un numero'));
 
     somma += n;
+}
+
+console.log(somma);*/
+
+
+//Ciclo WHILE
+
+let i = 0;
+let somma = 0;
+
+while (i < 5)  {
+    let n = parseInt(prompt('Inserisci un numero'));
+
+    somma += n;
+
+    i++;
 }
 
 console.log(somma);
