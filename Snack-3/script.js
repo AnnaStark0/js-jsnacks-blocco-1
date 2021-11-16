f56bb8614ac512894b74b89e53962d65c3ac24f6
Annastark <89winteriscoming@gmail.com>
2021-11-16 22:13:18 +0100

    Fine. (Post Correzione Mattutina)

--- Snack-3/script.js
+++ Snack-3/script.js
@@ -1,12 +1,38 @@
 //JSnack 3
 /*Stampa le potenze di 2 fino a 1000.*/
 
-let n = 2;
+
+//Mia soluzione sbagliata
+/*let n = 2;
 let power;
 
 for (let n = 2; n < 10 ; n++) {
    power = Math.pow(2, 2);
 }
-console.log(power);
+console.log(power);*/
 
 //--BOH--
+
+
+//Correzione 1
+
+/*let i = 1;
+
+let risPotenza = 1;
+
+while (risPotenza < 1000) {
+   console.log(risPotenza);
+   
+   risPotenza = Math.pow (2 , i);
+   i++;
+   
+}*/
+
+
+//Correzione 2
+
+let risPotenza = 1;
+while (risPotenza < 1000) {
+   console.log(risPotenza);
+   risPotenza *= 2;
+}
